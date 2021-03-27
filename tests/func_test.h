a006c6855742d3e9ed54f18dfaa3db0a3d2856cd
semen1994i <77006294+semen1994i@users.noreply.github.com>
2021-03-27 20:34:31 +0300

    Update func_test.h

--- tests/func_test.h
+++ tests/func_test.h
@@ -32,6 +32,6 @@ TEST(Func, test5_negative){
 }
 
 TEST(Func, test6_negative){
-    solution res = Func(0, 0, 0);
-    EXPECT_EQ(0, res);
+    solution res = Func(0, 0, 1);
+    EXPECT_EQ(0, res.count);
 }
