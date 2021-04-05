d16da7a064ffa8a5e7baf3928c232d9960d10a75
semen1994i <77006294+semen1994i@users.noreply.github.com>
2021-04-05 12:35:02 +0300

    Update func.c

--- app/func.c
+++ app/func.c
@@ -3,24 +3,8 @@
 
 solution Func(double a, double b, double c) {
     solution res;
-
-    if (a == 0){
-	    if (b == 0){
-		    if (c == 0){ 
-			    res.count = 1111;
-			    return res;
-		    }
-		    else{
-			    res.count = 0;
-			    return res;
-		    }
-	    }
-	    else{
-		    res.x1 = -c / b;
-		    return res;
-	    }
-    }
-		
+	res.count = 0;
+	
     double d = b * b - 4 * a * c;
 
     if (d < 0.0){
