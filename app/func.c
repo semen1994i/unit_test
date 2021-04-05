6e8c43bb0a18b8a13dea313bef11c343c5120635
semen1994i <77006294+semen1994i@users.noreply.github.com>
2021-04-05 12:43:26 +0300

    Update func.c

--- app/func.c
+++ app/func.c
@@ -3,22 +3,33 @@
 
 solution Func(double a, double b, double c) {
     solution res;
-	res.count = 0;
 	
-    double d = b * b - 4 * a * c;
-
-    if (d < 0.0){
-	    res.count = 0;
-    }
+	if (a == 0){
+		if (b == 0){
+			if (c == 0){
+				res.count = 1111;
+			}
+			else{
+				res.count = 0;
+			}
+		}
+		else{
+			res.x1 = -c / b;
+		}
+	}
+	double d = b * b - 4 * a * c;
 	
-    if (d == 0.0) {
-	    res.x1 = -b/(2*a);
-	    res.count = 1;
-    }
-    if (d > 0.0) {
-	    res.x1 = (-b - sqrt(d)) / (2*a);
-	    res.x2 = (-b + sqrt(d)) / (2*a);
-	    res.count = 2;
-    }
-    return res;
+	if (d < 0.0){
+		res.count = 0;
+	}
+	if (d == 0.0) {
+		res.x1 = -b/(2*a);
+		res.count = 1;
+	}
+	if (d > 0.0) {
+		res.x1 = (-b - sqrt(d)) / (2*a);
+		res.x2 = (-b + sqrt(d)) / (2*a);
+		res.count = 2;
+	}
+	return res;
 }
