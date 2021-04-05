903a939c473405679934821105597824838ab1d1
semen1994i <77006294+semen1994i@users.noreply.github.com>
2021-04-05 12:47:03 +0300

    Update func.c

--- app/func.c
+++ app/func.c
@@ -8,13 +8,16 @@ solution Func(double a, double b, double c) {
 		if (b == 0){
 			if (c == 0){
 				res.count = 1111;
+				return res;
 			}
 			else{
 				res.count = 0;
+				return res;
 			}
 		}
 		else{
 			res.x1 = -c / b;
+			return res;
 		}
 	}
 	double d = b * b - 4 * a * c;
@@ -24,12 +27,10 @@ solution Func(double a, double b, double c) {
 	}
 	if (d == 0.0) {
 		res.x1 = -b/(2*a);
-		res.count = 1;
 	}
 	if (d > 0.0) {
 		res.x1 = (-b - sqrt(d)) / (2*a);
 		res.x2 = (-b + sqrt(d)) / (2*a);
-		res.count = 2;
 	}
 	return res;
 }
