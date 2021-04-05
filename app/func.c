95a1f1fff69db159aecf616d4711e087480b076c
semen1994i <77006294+semen1994i@users.noreply.github.com>
2021-04-05 12:30:03 +0300

    Update func.c

--- app/func.c
+++ app/func.c
@@ -24,15 +24,17 @@ solution Func(double a, double b, double c) {
     double d = b * b - 4 * a * c;
 
     if (d < 0.0){
-        res.count = 0;
+	    res.count = 0;
     }
 	
     if (d == 0.0) {
-        res.x1 = -b/(2*a);
+	    res.x1 = -b/(2*a);
+	    res.count = 1;
     }
     if (d > 0.0) {
-        res.x1 = (-b - sqrt(d)) / (2*a);
-        res.x2 = (-b + sqrt(d)) / (2*a);
+	    res.x1 = (-b - sqrt(d)) / (2*a);
+	    res.x2 = (-b + sqrt(d)) / (2*a);
+	    res.count = 2;
     }
     return res;
 }
